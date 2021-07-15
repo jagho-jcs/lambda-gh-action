167083ee3e807ef7fddbeba41888dfeeb544df1f
Yomi Ogunyinka <kbab6aoo@hotmail.com>
2021-07-15 11:15:44 +0100

    max value reduced to 10

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
 import * as _ from 'lodash';
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
-	constant max = 10;
-	constant val = _.random(max)
-	constant response = {
-		statusCode: 200,
-		body: `The random value (max ${max}) is: ${val}`,
-	};
-	return response;
+  const max = 10;
+  const val = _.random(max);
+  const response = {
+    statusCode: 200,
+    body: `The random value (max ${max}) is: ${val}`,
+  };
+  return response;
 };
